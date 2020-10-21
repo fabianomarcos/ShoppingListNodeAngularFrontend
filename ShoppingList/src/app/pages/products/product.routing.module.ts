b1920ad004a70eb7168621357a69c19429be4bac
Fabiano Marcos <fabiano@vexter.com.br>
2020-10-21 02:28:51 -0300

    form errors

--- ShoppingList/src/app/pages/products/product.routing.module.ts
+++ ShoppingList/src/app/pages/products/product.routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsFormComponent } from './product-form/product-form.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
- // { path: 'new', component: ProductFormComponent },
- // { path: ':id/edit', component: ProductFormComponent }
+  { path: 'new', component: ProductsFormComponent },
+  { path: ':id/edit', component: ProductsFormComponent }
 ];
 
 @NgModule({
--- ShoppingList/src/app/shared/shared.module.ts
+++ ShoppingList/src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { RouterModule } from "@angular/router";
 import { HeaderComponent } from './components/header/header.component';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { CardInformationComponent } from './components/card-information/card-information.component';
+import { ServerErrorMessagesComponent } from '../core/components/server-error-messages/server-error-messages.component';
+import { FormFieldErrorComponent } from '../core/components/form-field-error/form-field-error.component';
 
 @NgModule({
   imports: [
@@ -16,7 +18,9 @@ import { CardInformationComponent } from './components/card-information/card-inf
   declarations: [
     BreadCrumbComponent,
     CardInformationComponent,
+    FormFieldErrorComponent,
     HeaderComponent,
+    ServerErrorMessagesComponent
   ],
   exports: [
     CommonModule,
@@ -25,7 +29,9 @@ import { CardInformationComponent } from './components/card-information/card-inf
 
     BreadCrumbComponent,
     CardInformationComponent,
+    FormFieldErrorComponent,
     HeaderComponent,
+    ServerErrorMessagesComponent
   ]
 })
 export class SharedModule { }
